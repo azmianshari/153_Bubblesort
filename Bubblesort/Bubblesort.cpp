cfb15129023057f3a57aaa01bed8a3419d464cf2
azmianshari <azmianshari123@gmail.com>
2023-03-17 11:43:06 +0700

    int

--- Bubblesort/Bubblesort.cpp
+++ Bubblesort/Bubblesort.cpp
@@ -2,8 +2,21 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main()
+int a[20];
+int n;
+
+void input() {
+    while (true)
+        cout << "Masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << :"\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+}
 {
     std::cout << "Hello World!\n";
 }
